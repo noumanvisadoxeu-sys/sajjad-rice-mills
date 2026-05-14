57a343e8fff1a09ae778b5f380eb56ab5712ae61
Noumanmalik-Trigger <79131208+Noumanmalik-Trigger@users.noreply.github.com>
2026-05-14 19:53:53 +0500

    latest_update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,8 @@
-import MainLayout from "@/layout/MainLayout";
 import Home from "./Home.tsx";
 
 const Index = () => (
   <div className="min-h-screen">
-    <MainLayout>
-      <Home />
-    </MainLayout>
+    <Home />
   </div>
 );
 
